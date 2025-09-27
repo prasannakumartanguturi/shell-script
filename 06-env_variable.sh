fb56f9baf796977b9fb9112efc7dbd76adc6136c
prasanna <prasannakumardso@gmail.com>
2025-09-27 20:34:55 +0530

    commit msg

--- 06-env_variable.sh
+++ 06-env_variable.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # env viriable we have to declare in .bashrc file with export command.
+#and we have to do source .bashrc then only changes will reflect in that file
 echo "$env_variable"
