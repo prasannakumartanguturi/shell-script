c28ea2e710cfc2133ec35e9788e6e898d6e27041
prasanna <prasannakumardso@gmail.com>
2025-09-27 22:43:02 +0530

    message commit

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid != 0 ]; then 
+    echo "run with sudo access"
+    exit 1
+fi
+validate(){
+
+    if [$1 != 0 ]; then 
+        echo "installing $2 is fail"
+    else
+        echo "imstalling $2 is success"
+    fi
+}
+dnf list installed nginx -y #checking nginx is available or not
+    if [ $? != 0 ]; then
+        dnf install nginx -y
+        validate $? "nginx"
+    else
+        echo "nginx already installed"
+    fi 
+echo -e "$G task completed $N"
