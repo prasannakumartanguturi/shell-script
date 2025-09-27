9e63f231eec150405e0a6d2cc4f7749142a026fc
prasanna <prasannakumardso@gmail.com>
2025-09-27 21:59:16 +0530

    message commit

--- 08-installation.sh
+++ 08-installation.sh
@@ -5,5 +5,10 @@ if [ $user_id != 0 ]; then
     echo "run the command with user access"
 else    
     echo "installing nginx"
-    dnf install nginx -y
+    #dnf install nginx -y
+    dnf list installed nginx
+    if [ $? -ne 0 ]; then
+        dnf install nginx -y
+    else
+        echo "nginx already installedd"
 fi
